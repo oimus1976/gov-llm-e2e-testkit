480be6ad0ea4e491ad3a25049459fc4b1ce4e4fb
oimus1976 <oimus1976@gmail.com>
2025-12-23 11:59:50 +0900

    feat: add F8 runner (v0.1r+) and align F4 writers

---
+++ src/execution/__init__.py
@@ -0,0 +1,21 @@
+"""Execution engine package (single-question I/F and F8 runner)."""
+
+from src.execution.run_single_question import (
+    ChatPageProtocol,
+    SingleQuestionResult,
+    run_single_question,
+)
+from src.execution.run_f8_set1 import (
+    F8QuestionOutcome,
+    F8_SET1_QUESTIONS,
+    run_f8_set1,
+)
+
+__all__ = [
+    "ChatPageProtocol",
+    "SingleQuestionResult",
+    "run_single_question",
+    "F8QuestionOutcome",
+    "F8_SET1_QUESTIONS",
+    "run_f8_set1",
+]
